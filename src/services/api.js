9a0e31cd9c691ea7d07fe9b3a3f8806c62a73a7e
FranciscoBraaz <francisco_braaz@hotmail.com>
2023-01-15 23:28:40 -0300

    feat: add url api and redirects

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:2000"
+const BASE_URL = "https://chat-node-eyyl.onrender.com/"
 
 export const api = axios.create({
   baseURL: BASE_URL,
